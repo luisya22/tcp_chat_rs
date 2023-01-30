c605fd67948c109ff618f8d5844c4001203f62a7
luisya22 <lmatosgarcia2@gmail.com>
2023-01-30 09:36:57 -0400

    feat: chat server

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,45 @@
 use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpListener;
+use tokio::sync::broadcast;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
+    let (tx, rx) = broadcast::channel(10);
+
     loop {
         let (mut socket, addr) = listener.accept().await.unwrap();
+
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
         tokio::spawn(async move {
             let (mut reader, mut writer) = socket.split();
 
             let mut reader = BufReader::new(reader);
             let mut line = String::new();
-
             loop {
+                tokio::select! {
+                   result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
 
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-                if bytes_read == 0 {
-                    break;
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+
+                        if addr != other_addr {
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
                 }
-
-                writer.write_all(line.as_bytes()).await.unwrap();
-                line.clear();
             }
+
         });
-
-
-
     }
 
 
